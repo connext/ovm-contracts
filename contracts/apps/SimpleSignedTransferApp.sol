fa7398f62685fbb2264bc8857a599b6ff9b2072e
Kevin Ho <kevinjho1996@gmail.com>
2020-07-13 16:54:28 -0400

    working ECDSA recover in assembly

--- contracts/apps/SimpleSignedTransferApp.sol
+++ contracts/apps/SimpleSignedTransferApp.sol
@@ -52,7 +52,7 @@ contract SimpleSignedTransferApp is CounterfactualApp {
         returns (address)
     {
         return
-            ECDSA.recover(
+            recoverAddr(
                 keccak256(
                     abi.encodePacked(
                         "\x19\x01",
@@ -77,6 +77,42 @@ contract SimpleSignedTransferApp is CounterfactualApp {
                 ),
                 action.signature
             );
+    }
+
+    function recoverAddr(bytes32 digest, bytes memory signature)
+        internal
+        view
+        returns (address o)
+    {
+
+        // Divide the signature in r, s and v variables
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        // ecrecover takes the signature parameters, and the only way to get them
+        // currently is to use assembly.
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
+
+        assembly {
+            // define pointer
+            let p := mload(0x40)
+            // store data assembly-favouring ways
+            mstore(p, digest)
+            mstore(add(p, 0x20), v)
+            mstore(add(p, 0x40), r)
+            mstore(add(p, 0x60), s)
+            if iszero(staticcall(sub(gas, 2000), 0x01, p, 0x80, p, 0x20)) {
+                revert(0, 0)
+            }
+            // data
+            o := mload(p)
+        }
     }
 
     function applyAction(
