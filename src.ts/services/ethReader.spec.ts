c8ba7b5404065578b9c1e0766263a98e7947ecc6
LayneHaber <layne.haber@gmail.com>
2021-02-02 15:11:16 -0700

    fix ethReader tests

--- src.ts/services/ethReader.spec.ts
+++ src.ts/services/ethReader.spec.ts
@@ -2,66 +2,106 @@ import { TransferNames, RegisteredTransfer } from "@connext/vector-types";
 import { expect } from "@connext/vector-utils";
 import { AddressZero, Zero } from "@ethersproject/constants";
 import { Contract } from "@ethersproject/contracts";
-import { deployments } from "hardhat";
+import { parseEther } from "@ethersproject/units";
 import pino from "pino";
 
 import { alice, bob, chainIdReq, provider } from "../constants";
-import { getContract, createChannel } from "../utils";
+import { getOvmContract, createOvmChannel } from "../utils";
 
 import { EthereumChainReader } from "./ethReader";
 
 // TODO: check whether result is valid, not just whether it exists
 describe("EthereumChainReader", function () {
   this.timeout(120_000);
-  const assetId = AddressZero;
   const transfer = {} as any; // TODO
   let chainId: number;
   let chainReader: EthereumChainReader;
   let channel: Contract;
   let factory: Contract;
+  let token: Contract;
   let transferRegistry: Contract;
+  let assetId: string;
 
   before(async () => {
-    await deployments.fixture(); // Start w fresh deployments
+    const mastercopy = await getOvmContract("ChannelMastercopy", alice);
+    factory = await getOvmContract("ChannelFactory", alice, [
+      mastercopy.address,
+      0,
+    ]);
+    transferRegistry = await getOvmContract("TransferRegistry", alice);
+    const hashlock = await getOvmContract("HashlockTransfer", alice);
+    await (
+      await transferRegistry.addTransferDefinition(
+        await hashlock.getRegistryInformation()
+      )
+    ).wait();
 
-    factory = await getContract("ChannelFactory", alice);
-    transferRegistry = await getContract("TransferRegistry", alice);
-
-    channel = (await createChannel()).connect(alice);
+    channel = (
+      await createOvmChannel(alice.address, bob.address, factory)
+    ).connect(alice);
     chainId = await chainIdReq;
     chainReader = new EthereumChainReader({ [chainId]: provider }, pino());
+
+    token = await getOvmContract("TestToken", alice);
+    await (await token.mint(alice.address, parseEther("0.001"))).wait();
+    assetId = token.address;
   });
 
   it("getChannelOnchainBalance", async () => {
-    const balance = (await chainReader.getChannelOnchainBalance(channel.address, chainId, assetId)).getValue();
+    const balance = (
+      await chainReader.getChannelOnchainBalance(
+        channel.address,
+        chainId,
+        assetId
+      )
+    ).getValue();
     expect(balance).to.equal(Zero);
   });
 
   it("getTotalDepositedA", async () => {
-    const res = await chainReader.getTotalDepositedA(channel.address, chainId, assetId);
+    const res = await chainReader.getTotalDepositedA(
+      channel.address,
+      chainId,
+      assetId
+    );
     expect(res.isError).to.be.false;
     const val = res.getValue();
     expect(val).to.be.ok;
   });
 
   it("getTotalDepositedB", async () => {
-    const res = (await chainReader.getTotalDepositedB(channel.address, chainId, assetId)).getValue();
+    const res = (
+      await chainReader.getTotalDepositedB(channel.address, chainId, assetId)
+    ).getValue();
     expect(res).to.be.ok;
   });
 
   it("getChannelFactoryBytecode", async () => {
-    const res = (await chainReader.getChannelFactoryBytecode(factory.address, chainId)).getValue();
+    const res = (
+      await chainReader.getChannelFactoryBytecode(factory.address, chainId)
+    ).getValue();
     expect(res).to.be.ok;
   });
 
   it("getChannelAddress", async () => {
-    const res = (await chainReader.getChannelAddress(alice.address, bob.address, factory.address, chainId)).getValue();
+    const res = (
+      await chainReader.getChannelAddress(
+        alice.address,
+        bob.address,
+        factory.address,
+        chainId
+      )
+    ).getValue();
     expect(res).to.be.ok;
   });
 
   it("getRegisteredTransferByName / getRegisteredTransferByDefinition", async () => {
     const byName = (
-      await chainReader.getRegisteredTransferByName(TransferNames.Withdraw, transferRegistry.address, chainId)
+      await chainReader.getRegisteredTransferByName(
+        TransferNames.HashlockTransfer,
+        transferRegistry.address,
+        chainId
+      )
     ).getValue();
     const chain = await transferRegistry.getTransferDefinitions();
     const cleaned = chain.map((r: RegisteredTransfer) => {
@@ -73,10 +113,16 @@ describe("EthereumChainReader", function () {
         encodedCancel: r.encodedCancel,
       };
     });
-    const info = cleaned.find((i: any) => i.name === TransferNames.Withdraw);
+    const info = cleaned.find(
+      (i: any) => i.name === TransferNames.HashlockTransfer
+    );
     expect(byName).to.be.deep.eq(info);
     const byDefinition = (
-      await chainReader.getRegisteredTransferByDefinition(byName.definition, transferRegistry.address, chainId)
+      await chainReader.getRegisteredTransferByDefinition(
+        byName.definition,
+        transferRegistry.address,
+        chainId
+      )
     ).getValue();
     expect(byDefinition).to.be.deep.eq(byName);
   });
@@ -92,7 +138,10 @@ describe("EthereumChainReader", function () {
         encodedCancel: r.encodedCancel,
       };
     });
-    const result = await chainReader.getRegisteredTransfers(transferRegistry.address, chainId);
+    const result = await chainReader.getRegisteredTransfers(
+      transferRegistry.address,
+      chainId
+    );
     expect(result.getError()).to.be.undefined;
     expect(result.getValue()).to.be.deep.eq(cleaned);
   });
@@ -104,7 +153,7 @@ describe("EthereumChainReader", function () {
         transfer.balance,
         transfer.transferDefinition,
         transferRegistry.address,
-        chainId,
+        chainId
         // bytecode?: string,
       )
     ).getValue();
@@ -115,7 +164,7 @@ describe("EthereumChainReader", function () {
     const res = (
       await chainReader.resolve(
         transfer,
-        chainId,
+        chainId
         // bytecode?: string,
       )
     ).getValue();
@@ -123,7 +172,9 @@ describe("EthereumChainReader", function () {
   });
 
   it("getCode", async () => {
-    const res = (await chainReader.getCode(channel.address, chainId)).getValue();
+    const res = (
+      await chainReader.getCode(channel.address, chainId)
+    ).getValue();
     expect(res).to.be.ok;
   });
 });
