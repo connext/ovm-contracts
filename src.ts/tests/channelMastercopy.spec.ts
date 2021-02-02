bde7975e8e5fae010320452bcf9d71a989d94c52
LayneHaber <layne.haber@gmail.com>
2021-02-02 14:59:24 -0700

    mastercopy passing

--- src.ts/tests/channelMastercopy.spec.ts
+++ src.ts/tests/channelMastercopy.spec.ts
@@ -1,18 +1,16 @@
 import { expect } from "@connext/vector-utils";
 import { AddressZero, HashZero, Zero } from "@ethersproject/constants";
 import { Contract } from "@ethersproject/contracts";
-import { deployments } from "hardhat";
 
 import { alice } from "../constants";
-import { getContract } from "../utils";
+import { getOvmContract } from "../utils";
 
 describe("ChannelMastercopy", function () {
   this.timeout(120_000);
   let mastercopy: Contract;
 
   beforeEach(async () => {
-    await deployments.fixture(); // Start w fresh deployments
-    mastercopy = await getContract("ChannelMastercopy", alice);
+    mastercopy = await getOvmContract("ChannelMastercopy", alice);
   });
 
   it("should deploy without error", async () => {
@@ -24,8 +22,29 @@ describe("ChannelMastercopy", function () {
       [Zero, Zero],
       [AddressZero, AddressZero],
     ];
-    const WithdrawDataZero = [AddressZero, AddressZero, AddressZero, Zero, Zero, AddressZero, Zero, "0x"];
-    const CoreChannelStateZero = [AddressZero, AddressZero, AddressZero, [], [], [], [], [], Zero, Zero, HashZero];
+    const WithdrawDataZero = [
+      AddressZero,
+      AddressZero,
+      AddressZero,
+      Zero,
+      Zero,
+      AddressZero,
+      Zero,
+      "0x",
+    ];
+    const CoreChannelStateZero = [
+      AddressZero,
+      AddressZero,
+      AddressZero,
+      [],
+      [],
+      [],
+      [],
+      [],
+      Zero,
+      Zero,
+      HashZero,
+    ];
     const CoreTransferStateZero = [
       AddressZero,
       HashZero,
@@ -61,18 +80,29 @@ describe("ChannelMastercopy", function () {
       { name: "getChannelDispute", args: [] },
       { name: "getDefundNonce", args: [AddressZero] },
       { name: "getTransferDispute", args: [HashZero] },
-      { name: "disputeChannel", args: [CoreChannelStateZero, HashZero, HashZero] },
-      { name: "defundChannel", args: [CoreChannelStateZero, [AddressZero], [Zero]] },
+      {
+        name: "disputeChannel",
+        args: [CoreChannelStateZero, HashZero, HashZero],
+      },
+      {
+        name: "defundChannel",
+        args: [CoreChannelStateZero, [AddressZero], [Zero]],
+      },
       { name: "disputeTransfer", args: [CoreTransferStateZero, []] },
-      { name: "defundTransfer", args: [CoreTransferStateZero, HashZero, HashZero, HashZero] },
+      {
+        name: "defundTransfer",
+        args: [CoreTransferStateZero, HashZero, HashZero, HashZero],
+      },
     ]) {
-      await expect(mastercopy[method.name](...method.args)).to.be.revertedWith("Mastercopy: ONLY_VIA_PROXY");
+      await expect(mastercopy[method.name](...method.args)).to.be.revertedWith(
+        "Mastercopy: ONLY_VIA_PROXY"
+      );
     }
   });
 
   it("should revert if sent eth bc it's the mastercopy", async () => {
-    await expect(alice.sendTransaction({ to: mastercopy.address, value: Zero })).to.be.revertedWith(
-      "Mastercopy: ONLY_VIA_PROXY",
-    );
+    await expect(
+      alice.sendTransaction({ to: mastercopy.address, value: Zero })
+    ).to.be.revertedWith("Mastercopy: ONLY_VIA_PROXY");
   });
 });
