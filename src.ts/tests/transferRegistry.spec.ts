cf26681231359993ce128446688cc7bba2ba88ec
LayneHaber <layne.haber@gmail.com>
2021-02-02 15:01:42 -0700

    registry tests passing

--- src.ts/tests/transferRegistry.spec.ts
+++ src.ts/tests/transferRegistry.spec.ts
@@ -5,34 +5,32 @@ import { expect } from "chai";
 import { deployments } from "hardhat";
 
 import { alice, rando } from "../constants";
-import { getContract } from "../utils";
+import { getOvmContract } from "../utils";
 
-describe("TransferRegistry.sol", function () {
+describe.only("TransferRegistry.sol", function () {
   this.timeout(120_000);
   let transfer: Contract;
   let registry: Contract;
   let registryInfo: RegisteredTransfer;
 
   beforeEach(async () => {
-    await deployments.fixture(); // Start w fresh deployments
-    registry = await getContract("TransferRegistry", alice);
-    // Just for these tests, reset the registry to it's initial state
-    await (await registry.removeTransferDefinition("HashlockTransfer")).wait();
-    await (await registry.removeTransferDefinition("Withdraw")).wait();
-    transfer = await getContract("HashlockTransfer", alice);
+    registry = await getOvmContract("TransferRegistry", alice);
+    transfer = await getOvmContract("HashlockTransfer", alice);
     registryInfo = await transfer.getRegistryInformation();
   });
 
   describe("addTransferDefinition", () => {
     it("should work", async () => {
       await (await registry.addTransferDefinition(registryInfo)).wait();
-      expect(await registry.getTransferDefinitions()).to.be.deep.eq([registryInfo]);
+      expect(await registry.getTransferDefinitions()).to.be.deep.eq([
+        registryInfo,
+      ]);
     });
 
     it("should fail IFF not called by the owner", async () => {
-      await expect(registry.connect(rando).addTransferDefinition(registryInfo)).revertedWith(
-        "Ownable: caller is not the owner",
-      );
+      await expect(
+        registry.connect(rando).addTransferDefinition(registryInfo)
+      ).revertedWith("Ownable: caller is not the owner");
     });
   });
 
@@ -42,14 +40,16 @@ describe("TransferRegistry.sol", function () {
     });
 
     it("should work", async () => {
-      await (await registry.removeTransferDefinition("HashlockTransfer")).wait();
+      await (
+        await registry.removeTransferDefinition("HashlockTransfer")
+      ).wait();
       expect(await registry.getTransferDefinitions()).to.be.deep.eq([]);
     });
 
     it("should fail IFF not called by the owner", async () => {
-      await expect(registry.connect(rando).removeTransferDefinition(transfer.address)).revertedWith(
-        "Ownable: caller is not the owner",
-      );
+      await expect(
+        registry.connect(rando).removeTransferDefinition(transfer.address)
+      ).revertedWith("Ownable: caller is not the owner");
     });
   });
 });
